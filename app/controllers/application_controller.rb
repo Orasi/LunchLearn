d9a0ad4ca2a6f90f0a27328c90439e9d8b617044
Matt Watson <Matt.Watson@orasi.com>
2014-07-11 20:14:47 -0400

    updated test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,11 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :require_login
   helper_method :current_user 
+  before_action :set_locale
+ 
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
+  end
 
   def current_user
     @_current_user ||= session[:current_user_id] &&
